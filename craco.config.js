d0dd6939925ce0bc744ef450902f4175367f3130
willpiam <willdoyle422@gmail.com>
2026-04-13 12:22:43 -0400

    debug pt 2

--- craco.config.js
+++ craco.config.js
@@ -26,6 +26,7 @@ module.exports = {
           process: require.resolve('process/browser'),
           path: require.resolve('path-browserify'),
           fs: false,
+          vm: false,
         },
       };
 
